907f750fd6f288095093cf3572f648a3c9ed9a05
Vashon Gonzales <vashon@kaped.io>
2022-10-28 22:22:25 -0700

    initial

---
+++ const/yourDetails.js
@@ -0,0 +1,5 @@
+// Replace this with your contract address
+export const contractAddress = "0x4994ca9254ACf7dEC94947281bafeCEe85A41e6e";
+
+// Replace this with your domain name.
+export const domainName = "partner.kaped.io";
